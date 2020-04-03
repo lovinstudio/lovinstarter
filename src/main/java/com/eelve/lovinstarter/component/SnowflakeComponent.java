39e62c311c261455fa6b40f3c125aa4caa476241
eelve <zzlve@foxmail.com>
2020-04-03 17:34:00 +0800

    修改雪花算法配置

--- src/main/java/com/eelve/lovinstarter/component/SnowflakeComponent.java
+++ src/main/java/com/eelve/lovinstarter/component/SnowflakeComponent.java
@@ -2,6 +2,7 @@ package com.eelve.lovinstarter.component;
 
 import com.eelve.lovinstarter.model.SnowflakeIdWorker;
 import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -13,9 +14,12 @@ import org.springframework.stereotype.Component;
  * @Date 2020/4/3 14:17
  * @Version 1.0
  **/
-@Log
+
 @Component
+@Log
 public class SnowflakeComponent {
+
+
     @Value("${server.datacenterId}")
     private long datacenterId;
 
--- src/test/java/com/eelve/lovinstarter/LovinstarterApplicationTests.java
+++ src/test/java/com/eelve/lovinstarter/LovinstarterApplicationTests.java
@@ -1,18 +1,25 @@
 package com.eelve.lovinstarter;
 
 import com.eelve.lovinstarter.utils.SnowflakeUtil;
+import lombok.extern.java.Log;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Log
 public class LovinstarterApplicationTests {
+
 
     @Test
     public void contextLoads() {
-        System.out.println(SnowflakeUtil.nextId());
+        log.info("----------------------");
+        log.info(SnowflakeUtil.nextId()+"");
     }
 
 }
