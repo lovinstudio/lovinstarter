3209b05f1c94a618da5febaf7119679e16f4c435
eelve <zzlve@foxmail.com>
2020-05-22 22:45:06 +0800

    添加自定义刷新配置

--- src/main/java/com/eelve/lovinstarter/config/GlobalConfig.java
+++ src/main/java/com/eelve/lovinstarter/config/GlobalConfig.java
@@ -2,10 +2,14 @@ package com.eelve.lovinstarter.config;
 
 
 
+import com.eelve.lovinstarter.constant.ConfigConstants;
+import com.eelve.lovinstarter.model.SystemDict;
 import com.eelve.lovinstarter.service.IGlobalConfigService;
 import com.eelve.lovinstarter.utils.SpringUtils;
 
+import java.lang.reflect.Field;
 import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @ClassName GlobalConfig
@@ -16,15 +20,15 @@ import java.util.Vector;
  **/
 public class GlobalConfig {
 
-    private static IGlobalConfigService bean = SpringUtils.getBean(IGlobalConfigService.class);
+    private static final IGlobalConfigService bean = SpringUtils.getBean(IGlobalConfigService.class);
 
 
     private static GlobalConfig instance = null;
-    private Vector  properties = null;
+    private ConcurrentHashMap<String ,Object> properties = null;
     private GlobalConfig() {
         //Load configuration information from DB or file
         //Set values for properties
-        properties = bean.getConfig();
+        properties = convert();
     }
     private static synchronized void syncInit() {
         if (instance == null) {
@@ -37,13 +41,41 @@ public class GlobalConfig {
         }
         return instance;
     }
-    public Vector getProperties() {
+    public ConcurrentHashMap<String ,Object> getProperties() {
         return properties;
     }
+
+    /**
+     * 刷新配置
+     */
     public void updateProperties() {
         //Load updated configuration information by new a GlobalConfig object
         GlobalConfig shadow = new GlobalConfig();
         properties = shadow.getProperties();
+    }
+
+    /**
+     * 参数转换方法
+     */
+    private ConcurrentHashMap<String ,Object> convert() {
+        ConcurrentHashMap<String ,Object> convert = new ConcurrentHashMap<>();
+        Vector<SystemDict> systemDictVector = bean.getConfig();
+        Class clazz = null;
+        try {
+            clazz = Class.forName("com.eelve.lovinstarter.constant.ConfigConstants");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        assert clazz != null;
+        Field[] fields = clazz.getDeclaredFields();
+        for (SystemDict systemDict : systemDictVector) {
+            for (Field field : fields) {
+                if (systemDict.getDict_key().toUpperCase().equals(field.getName())) {
+                    convert.put(field.getName(), systemDict.getDict_value());
+                }
+            }
+        }
+        return convert;
     }
 }
 
---
+++ src/main/java/com/eelve/lovinstarter/constant/ConfigConstants.java
@@ -0,0 +1,17 @@
+package com.eelve.lovinstarter.constant;
+
+/**
+ * @author zhao.zhilue
+ * @Description:
+ * @date 2020/5/22 22:22
+ */
+public enum ConfigConstants {
+    /**
+     * 应用id
+     */
+    APPID,
+    /**
+     * 密钥
+     */
+    KEY;
+}
