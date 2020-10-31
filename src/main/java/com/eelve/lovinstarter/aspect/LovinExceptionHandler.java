d8be9d9502a87b0bdf90dc1561b0518c226475fb
eelve <zzlve@foxmail.com>
2020-10-31 20:42:20 +0800

    添加可校验的List包装类

--- src/main/java/com/eelve/lovinstarter/aspect/LovinExceptionHandler.java
+++ src/main/java/com/eelve/lovinstarter/aspect/LovinExceptionHandler.java
@@ -3,8 +3,16 @@ package com.eelve.lovinstarter.aspect;
 import com.eelve.lovinstarter.exception.LovinException;
 import com.eelve.lovinstarter.vo.JsonResult;
 import lombok.extern.java.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 /**
  * @ClassName LovinExceptionHandler
@@ -38,5 +46,21 @@ public class LovinExceptionHandler {
     public JsonResult<Void> handleException(Exception e) {
         log.info(e.getMessage());
         return JsonResult.error();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST) //设置状态码为 400
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public String paramExceptionHandler(MethodArgumentNotValidException e) {
+        BindingResult exceptions = e.getBindingResult();
+        // 判断异常中是否有错误信息，如果存在就使用异常中的消息，否则使用默认消息
+        if (exceptions.hasErrors()) {
+            List<ObjectError> errors = exceptions.getAllErrors();
+            if (!errors.isEmpty()) {
+                // 这里列出了全部错误参数，按正常逻辑，只需要第一条错误即可
+                FieldError fieldError = (FieldError) errors.get(0);
+                return fieldError.getDefaultMessage();
+            }
+        }
+        return "请求参数错误";
     }
 }
